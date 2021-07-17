1fcde4450a459635fe5eb8ff2a7c34a8cfb47a11
dgraselli <dgraselli@gmail.com>
2021-07-17 05:15:11 -0300

    first test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 """
 # Welcome to Streamlit!
@@ -17,22 +20,10 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    var = st.slider("Varianza", 0, 10, 1)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    arr = np.random.normal(1, var, size=1000)
+    fig, ax = plt.subplots()
+    ax.hist(arr, bins=20)
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    st.pyplot(fig, width=20)  
